e3dbbbbe61e5c16754be445f355b06714a7e8fda
Tejas G <134586676+tejasghadge4@users.noreply.github.com>
2025-05-28 08:48:32 +0530

    Delete txt_viewer_project directory

--- txt_viewer_project/viewer/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('check_file/', views.check_file, name='check_file'),
-]
--- txt_viewer_project/viewer/utils.py
+++
@@ -1,7 +0,0 @@
-def get_google_drive_link_map():
-    return {
-        "test1.txt": "https://drive.google.com/file/d/1PfpgCTclrrLFwn5FnK7BQKndzMnXXok4/view?usp=drive_link",
-        "test2.txt": "https://drive.google.com/file/d/16CpTDEE_JmH4T0ndSbTXVnTyJ1qDUChn/view?usp=drive_link",
-        "test3.txt":"https://drive.google.com/file/d/1D0DlSUzKL4V-a1hdJ02cem7smaqQ03z-/view?usp=drive_link",
-        "test4.txt":"https://drive.google.com/file/d/1WkCAYMzb2wa-G4jX2B-ZmY87eAFl9VRQ/view?usp=drive_link",
-    }
